68004e13d7d4f829906a658eab9206ff3ac63336
Omid Raha <or@omidraha.com>
2022-08-16 16:26:46 +0430

    Fixed get_loggable_request_message error( message = self.get_loggable_request_message(request) TypeError: get_loggable_request_message() missing 1 required positional argument: 'response')

--- demo/app/middleware.py
+++ demo/app/middleware.py
@@ -6,13 +6,10 @@ class CustomSCIMAuthCheckMiddleware(SCIMAuthCheckMiddleware):
     An example of overriding the SCIM middleware to log more data
     around each request.
     """
-    def get_loggable_request_message(self, request, response):
-        body = self.get_loggable_request_body(request)
-
+    def get_loggable_request_message(self, request):
+        body = self.get_loggable_content(request.body)
         return {
             'request_absolute_uri': request.build_absolute_uri(),
             'request_method': request.method,
             'body': body,
-            'response_status_code': response.status_code,
-            'company_id': company_id,
         }
